7a2e7dc537763b42cf318b64c479d2cfb802976d
davepmiller <dpmdavemiller@gmail.com>
2019-07-18 09:07:16 -0600

    Add elapsedMillis

---
+++ src/main/java/metric/Metric.java
@@ -0,0 +1,13 @@
+package metric;
+
+import java.time.Instant;
+import java.time.Duration;
+
+public class Metric {
+    public static long elapsedMillis(Runnable r) {
+        Instant start = Instant.now();
+        r.run();
+        Instant finish = Instant.now();
+        return Duration.between(start, finish).toMillis();
+    }
+}
